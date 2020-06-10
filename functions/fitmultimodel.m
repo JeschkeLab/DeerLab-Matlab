a13b9d8eebf9434f45c74b3fe8915beb8217493a
Luis Fabregas <luisf@student.ethz.ch>
2020-06-10 11:22:30 +0200

    fitmultimodel: deleted unused code lines

--- functions/fitmultimodel.m
+++ functions/fitmultimodel.m
@@ -262,21 +262,6 @@ if nargin>3
     %Loop over different signals
     lb = zeros(numel(r),1);
     Pfitci = paramci.propagate(@(par)optModel(r,par(1:info.nparam)),lb,[]);
-    
-%     jacobian = jacobianest(@(par)optModel(r,par),param(1:info.nparam));
-%     % Calculate the confidence bands for the distance distribution
-%     modelvariance = arrayfun(@(idx)full(jacobian(idx,:))*covmatrix*full(jacobian(idx,:)).',1:numel(r)).';
-%     
-%     Pfitci = cell(numel(critical),1);
-%     for j=1:numel(critical)
-%         upperci = Pfit + critical(j)*sqrt(modelvariance);
-%         lowerci = max(0,Pfit - critical(j)*sqrt(modelvariance));
-%         Pfitci{j} = [upperci(:) lowerci(:)];
-%     end
-%     %Do not return a cell if only one confidence level is requested
-%     if numel(critical)==1
-%         Pfitci = Pfitci{1};
-%     end
 end
 
 if nargout>6
