250563f31271dab748244c39a4af3c0b6d17fb23
Luis Fabregas <luisf@student.ethz.ch>
2020-06-12 07:26:15 +0200

    correctzerotime: removed manual zero time correction option. Closes: #106

--- docsrc/source/functions/correctzerotime.rst
+++ docsrc/source/functions/correctzerotime.rst
@@ -17,18 +17,16 @@ Syntax
 .. code-block:: matlab
 
     tc = correctzerotime(V,t)
-    tc = correctzerotime(V,t,t0)
-    [tc,t0,pos] = correctzerotime(V,t,t0)
+    [tc,t0,idx] = correctzerotime(V,t)
 
 
 Parameters
     *   ``V`` - Signal (*N*-element array)
     *   ``t`` - Time axis (*N*-element array)
-    *   ``t0`` - Zero time (scalar)
 Returns
     *   ``tc`` - Corrected time axis (*N*-element array)
     *   ``t0`` - Zero time (scalar)
-    *   ``pos``  - Zero time index (scalar)
+    *   ``idx``  - Zero time index (scalar)
 
 
 -----------------------------
@@ -39,19 +37,9 @@ Description
 
 .. code-block:: matlab
 
-        [tc,t0,pos] = correctzerotime(V,t)
+        [tc,t0,idx] = correctzerotime(V,t)
 
-Determines the zero time of a dipolar signal ``V`` and corrects the time axis ``t`` (in microseconds) for it. If input signal ``V`` is the zero time ``t0`` and corrected time axis ``tc`` will be returned.
-
-
------------------------------
-
-
-.. code-block:: matlab
-
-    tc = correctzerotime(V,t,t0)
-
-Corrects the time axis ``t`` for a given zero-time ``t0`` in microseconds.
+Determines the zero time of a dipolar signal ``V`` and corrects the time axis ``t`` (in microseconds) for it. The function returns the corrected time axis ``tc``, the corresponding zero time ``t0`` and the index ``idx`` of the zero-time in the ``t`` array.
 
 
 
--- functions/correctzerotime.m
+++ functions/correctzerotime.m
@@ -6,10 +6,6 @@
 %   the time axis (t) for it, returning a corrected time axis (tc).
 %   If t is in ns/us, tc will be be in ns/us as well.
 %
-%   tc = CORRECTZEROTIME(V,t,t0)
-%   Corrects the time axis (t) for a given zero-time (t0), returning the
-%   corrected time axis.
-%
 %   [tc,t0,pos] = CORRECTZEROTIME(V,t)
 %   Returns the corrected time axis (tc), the zero-time (t0) in ns/us and the 
 %   zero-time array index (pos). 
@@ -20,19 +16,10 @@
 % Copyright(c) 2019-2020: Luis Fabregas, Stefan Stoll and other contributors.
 
 
-function [tcorr,t0,idxt0] = correctzerotime(V,t,t0)
+function [tcorr,t0,idxt0] = correctzerotime(V,t)
 
 if nargin<2
     error('Not enough input arguments.')
-end
-
-if nargin<3 || isempty(t0)
-    t0 = [];
-else
-    validateattributes(t0,{'numeric'},{'scalar','nonnegative'},mfilename,'ZeroTime')
-end
-if ~iscolumn(V)
-    V = V.';
 end
 if any(t<0)
    t = t + abs(min(t)); 
@@ -48,37 +35,36 @@ V = V(:);
 resolution = 4;
 tfine = linspace(min(t),max(t),(numel(t)-1)*resolution+1);
 Vfine = interp1(t,real(V),tfine,'spline');
-% get zero time, if not provided
-if isempty(t0)
-    % Determine maximum
-    [~,maxPos] = max(Vfine);
-    idxt0 = 1;
-    %If maximum is not the first or last point, then do moment analysis
-    if maxPos>1 && maxPos<length(Vfine)
-        % Determine the width of the interval to use for the integral
-        if maxPos<length(Vfine)/2
-          maxDelta = floor((maxPos-1)/2);
-        else
-          maxDelta = floor((length(Vfine)-maxPos)/2);
-        end
-        offsetrange = -maxDelta:maxDelta;
-        
-        %Look around the maximum for lowest moment integral
-        minIntegral = realmax;
-        idxt0 = maxPos;
-        for idx = maxPos-maxDelta:maxPos+maxDelta
-            %Query new candidate for zero position and integrate
-            dt = tfine(idx+offsetrange)-tfine(idx);
-            Integral = sum(Vfine(idx+offsetrange).*dt);
-            %If integral is lower than prior best, then update new candidate
-            if abs(Integral) < minIntegral
-                minIntegral = abs(Integral);
-                idxt0 = idx;
-            end
+
+% Determine maximum
+[~,maxPos] = max(Vfine);
+idxt0 = 1;
+%If maximum is not the first or last point, then do moment analysis
+if maxPos>1 && maxPos<length(Vfine)
+    % Determine the width of the interval to use for the integral
+    if maxPos<length(Vfine)/2
+        maxDelta = floor((maxPos-1)/2);
+    else
+        maxDelta = floor((length(Vfine)-maxPos)/2);
+    end
+    offsetrange = -maxDelta:maxDelta;
+    
+    %Look around the maximum for lowest moment integral
+    minIntegral = realmax;
+    idxt0 = maxPos;
+    for idx = maxPos-maxDelta:maxPos+maxDelta
+        %Query new candidate for zero position and integrate
+        dt = tfine(idx+offsetrange)-tfine(idx);
+        Integral = sum(Vfine(idx+offsetrange).*dt);
+        %If integral is lower than prior best, then update new candidate
+        if abs(Integral) < minIntegral
+            minIntegral = abs(Integral);
+            idxt0 = idx;
         end
     end
-    t0 = tfine(idxt0);
 end
+t0 = tfine(idxt0);
+
 
 % Correct time axis
 tcorr = t - t0;
